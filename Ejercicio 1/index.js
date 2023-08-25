397c24f832bdf3e1b601478f70874a7f50e71293
MauriBarrientos <mauribarrientos23@gmail.com>
2023-08-25 16:33:32 -0300

    Correccion de errores en ejercio 1

--- Ejercicio 1/index.js
+++ Ejercicio 1/index.js
@@ -1,6 +1,5 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
-
-.then(response => response.json())
+  .then(response => response.json())
 
 .then(data => {
   const elementosArray = data.slice(0, 3);
